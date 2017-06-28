728dafff562d6c7166043d59d33ad3dd66f39569
Marc-Olivier Bolduc <bolduc.marco21@gmail.com>
2017-06-28 16:25:20 -0400

    Add a RGB Struct for each pixel

--- Code/Reconnaissance/Reconnaissance/load_image.cpp
+++ Code/Reconnaissance/Reconnaissance/load_image.cpp
@@ -20,6 +20,13 @@ namespace {
 	const unsigned int IMAGE_SIZE = IMAGE_WIDTH * IMAGE_HEIGHT * 3;
 };
 
+struct pixel
+{
+	int r;
+	int g;
+	int b;
+};
+
 int main( int argc, char **argv)
 {
 	if(argc != 2)
@@ -27,9 +34,6 @@ int main( int argc, char **argv)
 		cerr << "Erreur: Vous devez specifier seulement l'image a charger" << endl;
 		return EXIT_FAILURE;
 	}
-
-	cout << "0 : " << argv[0] << endl;
-	cout << "1 : " << argv[1] << endl;
 
 	// Open image file
 	ifstream image_file(argv[1], ios::binary);
@@ -41,11 +45,7 @@ int main( int argc, char **argv)
 
 	// Check file size
 	image_file.seekg(0, image_file.end);
-	unsigned int a = (unsigned int)image_file.tellg();
-
-	cout << a << endl;
-
-	if(a != IMAGE_SIZE)
+	if((unsigned int)image_file.tellg() != IMAGE_SIZE)
 	{
 		cerr << "Erreur: La taille de l'image specifiee est incorrecte" << endl;
 		return EXIT_FAILURE+2;
@@ -62,5 +62,37 @@ int main( int argc, char **argv)
 	// L'outil convert de imagemagick peut etre interessant pour convertir le format d'image si requis:
 	// convert -depth 8 -size 480x480 test.rgb test.png
 
+	//int max = 0;
+	//int nb_max = 0;
+	int index = 0;
+	
+	//for (int i = 0; i < IMAGE_SIZE; i++) {
+	//	if (max < (int)image[i]) {
+	//		max = (int)image[i];
+	//	}
+	//}
+
+	//for (int i = 0; i < IMAGE_SIZE; i++) {
+	//	if ((int)image[i] == max) {
+	//		nb_max++;
+	//	}
+	//}
+
+	//cout << "Max=" << max << endl;
+	//cout << "Nb_max=" << nb_max << endl;
+
+	int pixels[480][480];
+
+	//for (int i = 0; i < IMAGE_HEIGHT; i++) {
+	//	for (int j = 0; j < IMAGE_WIDTH; j++) {
+	//		index = i * IMAGE_WIDTH + j * 3;
+	//		pixels[i][j].r = (int)image[index];
+	//		pixels[i][j].g = (int)image[index + 1];
+	//		pixels[i][j].b = (int)image[index + 2];
+	//	}
+	//}
+
+
+	system("pause");
 	return EXIT_SUCCESS;
 }
