3687242a46151a3846f59e8f6dd8c7af40727e15
MarcEric <medaragon@hotmail.com>
2017-07-04 15:27:16 -0400

    Recreation d'une image bleue
    On garde maintenant le shared_array initial, et on effectue un calcul afin d'aller chercher les pixels voulus

--- Code/Reconnaissance/Reconnaissance/load_image.cpp
+++ Code/Reconnaissance/Reconnaissance/load_image.cpp
@@ -18,16 +18,20 @@ namespace {
 	const unsigned int IMAGE_WIDTH = 480;
 	const unsigned int IMAGE_HEIGHT = 480;
 	const unsigned int IMAGE_SIZE = IMAGE_WIDTH * IMAGE_HEIGHT * 3;
+	const unsigned int RED = 0;
+	const unsigned int GREEN = 1;
+	const unsigned int BLUE = 2;
 };
 
-struct pixel
-{
-	int r;
-	int g;
-	int b;
-};
-
-pixel pixels[480][480];
+//struct pixel
+//{
+//	int r;
+//	int g;
+//	int b;
+//};
+//
+//pixel pixels[IMAGE_HEIGHT][IMAGE_WIDTH];
+unsigned int bluePic[IMAGE_SIZE];
 
 int main( int argc, char **argv)
 {
@@ -58,39 +62,53 @@ int main( int argc, char **argv)
 	boost::shared_array<uint8_t> image(new uint8_t[IMAGE_SIZE]);
 	image_file.read(reinterpret_cast<char *>(image.get()), IMAGE_SIZE);
 
+	// Aller chercher un pixel
+	image[(4 * IMAGE_WIDTH + 14) * 3 + BLUE];
+
+
+
+	// Create blue image
+	for (int i = 0; i < IMAGE_SIZE; i+=3) {
+		bluePic[i + RED] = 0;
+		bluePic[i + GREEN] = 0;
+		bluePic[i + BLUE] = image[i + BLUE];
+	}
+
+	FILE *imageFile;
+
+	imageFile = fopen("bluePic.ppm", "wb");
+	if (imageFile == NULL) {
+		perror("ERROR: Cannot open output file");
+		exit(EXIT_FAILURE);
+	}
+
+	fprintf(imageFile, "P3\n");               // P6 filetype
+	fprintf(imageFile, "%d %d\n", IMAGE_WIDTH, IMAGE_HEIGHT);   // dimensions
+	fprintf(imageFile, "255\n");              // Max pixel
+
+	for (int i = 0; i < IMAGE_SIZE; i+=3) {
+		fprintf(imageFile, "%d %d %d\n", bluePic[i], bluePic[i + 1], bluePic[i + 2]);
+	}
+	fclose(imageFile);
+
+
+
 	// Votre code ici: l'image est un tableau lineaire de uint8.
 	// Chaque pixel contient 3 uint8 soit les composantes: Red, Green, Blue (expliquant le "*3" dans IMAGE_SIZE)
 	// Les pixels sont sotckes en mode: row-major order.
 	// L'outil convert de imagemagick peut etre interessant pour convertir le format d'image si requis:
 	// convert -depth 8 -size 480x480 test.rgb test.png
 
-	//int max = 0;
-	//int nb_max = 0;
 	int index = 0;
-	
-	//for (int i = 0; i < IMAGE_SIZE; i++) {
-	//	if (max < (int)image[i]) {
-	//		max = (int)image[i];
+
+	//for (int i = 0; i < IMAGE_HEIGHT; i++) {
+	//	for (int j = 0; j < IMAGE_WIDTH; j++) {
+	//		index = i * IMAGE_WIDTH + j * 3;
+	//		pixels[i][j].r = (int)image[index];
+	//		pixels[i][j].g = (int)image[index + 1];
+	//		pixels[i][j].b = (int)image[index + 2];
 	//	}
 	//}
-
-	//for (int i = 0; i < IMAGE_SIZE; i++) {
-	//	if ((int)image[i] == max) {
-	//		nb_max++;
-	//	}
-	//}
-
-	//cout << "Max=" << max << endl;
-	//cout << "Nb_max=" << nb_max << endl;
-
-	for (int i = 0; i < IMAGE_HEIGHT; i++) {
-		for (int j = 0; j < IMAGE_WIDTH; j++) {
-			index = i * IMAGE_WIDTH + j * 3;
-			pixels[i][j].r = (int)image[index];
-			pixels[i][j].g = (int)image[index + 1];
-			pixels[i][j].b = (int)image[index + 2];
-		}
-	}
 
 	//system("pause");
 	return EXIT_SUCCESS;
