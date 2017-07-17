24462f23abd07284b2bd7959f46f3ce66681fdfc
MarcEric <medaragon@hotmail.com>
2017-07-17 16:24:29 -0400

    Changer les noms de variable et ajout de commentaires

--- Code/Matlab/Correlation.m
+++ Code/Matlab/Correlation.m
@@ -1,11 +1,19 @@
-function [posBille] = Correlation(image, bille, rayonBille, coinGaucheImage)
-        truc = normxcorr2(bille, image);
+function [posBille] = Correlation(image, bille, rayonBille, coinHautGaucheImage)
+        % Faire la correlation entre l'image de la bille et l'image
+        % courante
+        corr = normxcorr2(bille, image);
         
+        % Afficher la correlation
         figure
-        mesh(truc);
-        ind = find(truc==(max(max(truc))));
-        [posBilleDansCorrX posBilleDansCorrY] = ind2sub(size(truc),ind);
+        mesh(corr);
         
-        posBille(1) = round(posBilleDansCorrY + coinGaucheImage(1) - rayonBille);
-        posBille(2) = round(posBilleDansCorrX + coinGaucheImage(2) - rayonBille);
+        % Trouver l'indice de la bille dans l'image courante reduite
+        ind = find(corr==(max(max(corr))));
+        [x y] = ind2sub(size(corr),ind);
+        
+        % On trouve la position de la bille dans l'image originelle.
+        % L'indice trouve est celui dans le coin en bas a droite de la
+        % bille.
+        posBille(1) = round(y + coinHautGaucheImage(1) - rayonBille);
+        posBille(2) = round(x + coinHautGaucheImage(2) - rayonBille);
 end
